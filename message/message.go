e32a02abee063da02ed548af9b4c1509a0e32d79
Cockpit Cutie <cockpitcutie@gmail.com>
2025-11-08 21:21:43 +0100

    add json serialization

--- message/message.go
+++ message/message.go
@@ -1,5 +1,11 @@
 package message
 
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
 type Message interface {
 	ID() uint32
 	SetID(id uint32)
@@ -7,17 +13,26 @@ type Message interface {
 }
 
 type message struct {
-	id uint32
+	Id uint32
 }
 
 func (m message) ID() uint32 {
-	return m.id
+	return m.Id
 }
 
 func (m *message) SetID(id uint32) {
-	m.id = id
+	m.Id = id
 }
 
 func (m message) IsServerEvent() bool {
-	return m.id == 0
+	return m.Id == 0
+}
+
+func Serialize(m Message) (string, error) {
+	messageKind := reflect.TypeOf(m).Elem().Name()
+	serialized, err := json.Marshal(m)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf(`[{"%s":%s}]`, messageKind, string(serialized)), nil
 }
